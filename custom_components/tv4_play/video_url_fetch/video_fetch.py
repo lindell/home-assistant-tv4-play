2004f7374b493f6f5761015a576eb17900117c22
Johan Lindell <johan.lindell@lendo.se>
2019-11-15 13:51:02 +0100

    Fixed formating

--- custom_components/tv4_play/video_url_fetch/video_fetch.py
+++ custom_components/tv4_play/video_url_fetch/video_fetch.py
@@ -1,8 +1,9 @@
 from requests import get
 from requests import post
 
-# Get the CDN video url
+
 def get_video_url(video_asset):
+    "Get the CDN video url"
     url = 'https://playback-api.b17g.net/media/{}?service=tv4&device=browser&protocol=hls%2Cdash&drm=widevine&is_live={}&has_startover={}'.format(
         video_asset['id'],
         video_asset['live'],
@@ -11,13 +12,16 @@ def get_video_url(video_asset):
 
     data = get(url).json()
     if 'errorCode' in data:
-        raise Exception("Could not fetch the CDN data: {}".format(data['errorCode']))
+        raise Exception(
+            "Could not fetch the CDN data: {}".format(data['errorCode'])
+        )
     video_url = data['playbackItem']['manifestUrl']
 
     return video_url
 
-# Get information about the sugested episode based on the program name
+
 def get_suggested_episode(program_id):
+    "Get information about the suggested episode based on the program name"
     query = """
         query($name: String) {
             program(nid: $name) {
@@ -39,7 +43,8 @@ def get_suggested_episode(program_id):
         },
     }
 
-    data = post('https://tv4-graphql-web.b17g.net/graphql', json=query_data).json()
+    data = post('https://tv4-graphql-web.b17g.net/graphql',
+                json=query_data).json()
     video_asset = data['data']['program']['suggestedEpisode']['videoAsset']
 
     return video_asset
