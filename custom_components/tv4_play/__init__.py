d0777e76e49e166bc3f66222085bad1593965d05
Johan <johan@lindell.me>
2019-11-20 21:38:29 +0100

    Added tests and restructured

--- custom_components/tv4_play/__init__.py
+++ custom_components/tv4_play/__init__.py
@@ -1,8 +1,7 @@
 import logging
 import homeassistant.helpers.config_validation as cv
 import voluptuous as vol
-from requests import get
-from requests import post
+from .video_url_fetch.video_fetch import get_video_url, get_suggested_episode
 
 DOMAIN = "tv4_play"
 
@@ -19,48 +18,6 @@ SERVICE_PLAY_SUGGESTED_SCHEMA = vol.Schema({
 
 _LOGGER = logging.getLogger(__name__)
 
-# Get the CDN video url
-def get_video_url(video_asset):
-    url = 'https://playback-api.b17g.net/media/{}?service=tv4&device=browser&protocol=hls%2Cdash&drm=widevine&is_live={}&has_startover={}'.format(
-        video_asset['id'],
-        video_asset['live'],
-        video_asset['startOver'],
-    )
-
-    data = get(url).json()
-    if 'errorCode' in data:
-        raise Exception("Could not fetch the CDN data: {}".format(data['errorCode']))
-    video_url = data['playbackItem']['manifestUrl']
-
-    return video_url
-
-# Get information about the sugested episode based on the program name
-def get_suggested_episode(program_id):
-    query = """
-        query($name: String) {
-            program(nid: $name) {
-                suggestedEpisode {
-                    videoAsset {
-                        id
-                        live
-                        startOver
-                    }
-                }
-            }
-        }
-    """
-
-    query_data = {
-        'query': query,
-        'variables': {
-            'name': program_id,
-        },
-    }
-
-    data = post('https://tv4-graphql-web.b17g.net/graphql', json=query_data).json()
-    video_asset = data['data']['program']['suggestedEpisode']['videoAsset']
-
-    return video_asset
 
 async def async_setup(hass, config):
 
@@ -80,6 +37,7 @@ async def async_setup(hass, config):
 
         await hass.services.async_call('media_player', 'play_media', service_data)
 
-    hass.services.async_register(DOMAIN, SERVICE_PLAY_SUGGESTED, play_suggested, SERVICE_PLAY_SUGGESTED_SCHEMA)
+    hass.services.async_register(
+        DOMAIN, SERVICE_PLAY_SUGGESTED, play_suggested, SERVICE_PLAY_SUGGESTED_SCHEMA)
 
     return True
